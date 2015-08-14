40a58c9e1ca6dbaf5b120197c0bb7b6125c42e04
Kai Zhao <loverszhao@gmail.com>
2015-08-14 09:40:56 +0800

    hack 138

---
+++ 138/sol.cc
@@ -0,0 +1,116 @@
+#include <climits>
+#include <cmath>
+
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <map>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "utils.h"
+
+using std::cout;
+using std::endl;
+using std::list;
+using std::map;
+using std::stack;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	// Time  Complexcity: O(n)
+	// Space Complexcity: O(n)
+	RandomListNode *copyRandomList(RandomListNode *head) {
+		if (!head)
+			return NULL;
+
+		unordered_map<RandomListNode *, RandomListNode *> old_to_new;
+
+		RandomListNode *new_head = new RandomListNode(head->label);
+		RandomListNode *old_node = head;
+		RandomListNode *new_node = new_head;
+		RandomListNode *pre_node = new_head;
+
+		old_to_new[NULL] = NULL;
+		old_to_new[old_node] = new_node;
+		old_node = old_node->next;
+
+		while (old_node) {
+			new_node = new RandomListNode(old_node->label);
+
+			pre_node->next = new_node;
+			pre_node = new_node;
+
+			old_to_new[old_node] = new_node;
+
+			old_node = old_node->next;
+		}
+
+		old_node = head;
+		new_node = new_head;
+
+		while (old_node) {
+			new_node->random = old_to_new[old_node->random];
+
+			old_node = old_node->next;
+			new_node = new_node->next;
+		}
+		return new_head;
+	}
+};
+
+int main()
+{
+	Solution sln;
+
+{
+	RandomListNode n1(1), n2(2), n3(3), n4(4), n5(5), n6(6);
+
+	n1.next = &n2;
+	n2.next = &n3;
+	n3.next = &n4;
+	n4.next = &n5;
+	n5.next = &n6;
+
+	n1.random = &n3;
+	n2.random = &n1;
+	n3.random = &n6;
+	n4.random = &n5;
+	n5.random = &n5;
+	n6.random = NULL;
+
+	RandomListNode * new_head = sln.copyRandomList(&n1);
+	RandomListNode * new_node = new_head;
+
+	while (new_node) {
+		cout << "label=" << new_node->label << endl;
+		if (new_node->random == NULL)
+			cout << "random->label=NULL" << endl;
+		else
+			cout << "random->label=" << new_node->random->label << endl;
+		cout << endl;
+		new_node = new_node->next;
+	}
+}
+
+	return 0;
+}
