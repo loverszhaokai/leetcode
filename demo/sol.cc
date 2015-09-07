0a1cb990ef0d9887bdda2b909e236a23db904275
Kai Zhao <loverszhao@gmail.com>
2015-09-07 08:46:11 +0800

    Update demo

--- demo/sol.cc
+++ demo/sol.cc
@@ -25,5 +25,18 @@ int main()
 {
 	Solution sln;
 
+	const struct TestCase {
+	} test_cases [] = {
+	};
+
+	for (int iii = 0; iii < sizeof(test_cases) / sizeof(TestCase); iii++) {
+
+		if ( == ) {
+			cout << "Case #" << iii << " failed" << endl;
+			return -1;
+		}
+
+	}
+
 	return 0;
 }
